b45b3f9ccbf93a8616a3b3e4c1ea4f9baa36c3c1
Pocket Ilmatto <pocket.ilmatto@gmail.com>
2014-01-12 13:27:22 -0800

    Full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,19 @@ require 'spec_helper'
 describe "Static pages" do
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
-		end
-		it "should have the right title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Home")
-		end
+	      visit '/static_pages/home'
+	      expect(page).to have_content('Sample App')
+	    end
+
+	    it "should have the base title" do
+	      visit '/static_pages/home'
+	      expect(page).to have_title("Rails tutorial")
+	    end
+
+	    it "should not have a custom page title" do
+	      visit '/static_pages/home'
+	      expect(page).not_to have_title('| Home')
+	    end
 	end
 
 
